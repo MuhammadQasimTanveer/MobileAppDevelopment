6921cda36d4aa54c9c882c6c9d47e0ba54627151
MuhammadQasimTanveer <qasimtanvir.qt84@gmail.com>
2025-09-18 15:10:10 +0500

    upload Assig 1

--- assignment_1/lib/main.dart
+++ assignment_1/lib/main.dart
@@ -7,13 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bidding App',
-      theme: ThemeData(
-        // This is the theme of your application.
+      theme: ThemeData
+        (
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
